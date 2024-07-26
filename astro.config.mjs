cb3df5b8c20ca94b586348fab1c4312441db5c5f
Marc Pont <mpont91@gmail.com>
2024-07-26 09:41:59 +0200

    Install dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue(), tailwind(), react()],
+  output: "server",
+  adapter: vercel()
+});
